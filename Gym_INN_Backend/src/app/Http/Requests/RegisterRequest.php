bff133c6e77994fb5aba3443bb407096be0b6d0a
JonyTask <yuusu.hyb@gmail.com>
2024-08-09 16:41:17 +0900

    dev : auth foundation

--- Gym_INN_Backend/src/app/Http/Requests/RegisterRequest.php
+++ Gym_INN_Backend/src/app/Http/Requests/RegisterRequest.php
@@ -11,7 +11,7 @@ class RegisterRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class RegisterRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|string|unique:users|max:255',
+            'password' => 'required|string|min:8|max:255',
         ];
     }
 }
--- Gym_INN_Backend/src/routes/api.php
+++ Gym_INN_Backend/src/routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Actions\Fortify\RegisteredUserController;
+use App\Actions\Fortify\AuthenticateController;
 
-
-Route::middleware('auth:sanctum')->group(function () {
-    /* 認証必須ルート */
-});
+Route::post('/login', [AuthenticateController::class, 'store']);
+Route::post('/logout', [AuthenticateController::class, 'destroy']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
